d0d534afc8c177c74a4928444592a9cd672b729a
Alexod24 <alex.od2408@gmail.com>
2024-11-02 13:33:07 -0500

    feat: :zap: Cambios de Feedback

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,119 +3,163 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
-      text: 'Homes',
+      text: 'Sobre Nosotros',
       links: [
         {
-          text: 'SaaS',
-          href: getPermalink('/homes/saas'),
-        },
-        {
-          text: 'Startup',
-          href: getPermalink('/homes/startup'),
-        },
-        {
-          text: 'Mobile App',
-          href: getPermalink('/homes/mobile-app'),
-        },
-        {
-          text: 'Personal',
+          text: 'Historia',
           href: getPermalink('/homes/personal'),
         },
-      ],
-    },
-    {
-      text: 'Pages',
-      links: [
         {
-          text: 'Features (Anchor Link)',
-          href: getPermalink('/#features'),
+          text: 'Experiencia',
+          href: getPermalink('/homes/personal') + '#resume',
         },
+        
         {
-          text: 'Services',
-          href: getPermalink('/services'),
-        },
-        {
-          text: 'Pricing',
-          href: getPermalink('/pricing'),
-        },
-        {
-          text: 'About us',
-          href: getPermalink('/about'),
-        },
-        {
-          text: 'Contact',
-          href: getPermalink('/contact'),
-        },
-        {
-          text: 'Terms',
+          text: 'Terminos',
           href: getPermalink('/terms'),
         },
         {
-          text: 'Privacy policy',
+          text: 'Privacidad y Politica',
           href: getPermalink('/privacy'),
         },
+       
       ],
     },
     {
-      text: 'Landing',
+      text: 'Servicios',
       links: [
         {
-          text: 'Lead Generation',
-          href: getPermalink('/landing/lead-generation'),
+          text: 'Características',
+          href: getPermalink('/#features'),
         },
         {
-          text: 'Long-form Sales',
-          href: getPermalink('/landing/sales'),
+          text: 'Soluciones',
+          href: getPermalink('/#soluciones'),
         },
         {
-          text: 'Click-Through',
-          href: getPermalink('/landing/click-through'),
+          text: 'Servicios',
+          href: getPermalink('/#servicios'),
         },
-        {
-          text: 'Product Details (or Services)',
-          href: getPermalink('/landing/product'),
-        },
-        {
-          text: 'Coming Soon or Pre-Launch',
-          href: getPermalink('/landing/pre-launch'),
-        },
-        {
-          text: 'Subscription',
-          href: getPermalink('/landing/subscription'),
-        },
+        
+        
       ],
     },
     {
-      text: 'Blog',
+      text: 'Primeros pasos',
       links: [
         {
-          text: 'Blog List',
-          href: getBlogPermalink(),
+          text: 'Como iniciar',
+          href: getPermalink('/#pasos'),
         },
-        {
-          text: 'Article',
-          href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
-        },
-        {
-          text: 'Article (with MDX)',
-          href: getPermalink('markdown-elements-demo-post', 'post'),
-        },
-        {
-          text: 'Category Page',
-          href: getPermalink('tutorials', 'category'),
-        },
-        {
-          text: 'Tag Page',
-          href: getPermalink('astro', 'tag'),
-        },
+        
       ],
     },
+    // {
+    //   text: 'Blog',
+    //   links: [
+    //     {
+    //       text: 'Blog List',
+    //       href: getBlogPermalink(),
+    //     },
+    //     {
+    //       text: 'Article',
+    //       href: getPermalink('get-started-website-with-astro-tailwind-css', 'post'),
+    //     },
+    //     {
+    //       text: 'Article (with MDX)',
+    //       href: getPermalink('markdown-elements-demo-post', 'post'),
+    //     },
+    //     {
+    //       text: 'Category Page',
+    //       href: getPermalink('tutorials', 'category'),
+    //     },
+    //     {
+    //       text: 'Tag Page',
+    //       href: getPermalink('astro', 'tag'),
+    //     },
+    //     {
+    //       text: 'SaaS',
+    //       href: getPermalink('/homes/saas'),
+    //     },
+    //     {
+    //       text: 'Startup',
+    //       href: getPermalink('/homes/startup'),
+    //     },
+    //     {
+    //       text: 'Mobile App',
+    //       href: getPermalink('/homes/mobile-app'),
+    //     },
+    //     {
+    //       text: 'Services',
+    //       href: getPermalink('/services'),
+    //     },
+    //     {
+    //       text: 'Pricing',
+    //       href: getPermalink('/pricing'),
+    //     },
+    //     {
+    //       text: 'About us',
+    //       href: getPermalink('/about'),
+    //     },
+    //     {
+    //       text: 'Contact',
+    //       href: getPermalink('/contact'),
+    //     },
+    //     {
+    //       text: 'Terms',
+    //       href: getPermalink('/terms'),
+    //     },
+    //     {
+    //       text: 'Privacy policy',
+    //       href: getPermalink('/privacy'),
+    //     },
+    //     {
+    //       text: 'Long-form Sales',
+    //       href: getPermalink('/landing/sales'),
+    //     },
+    //     {
+    //       text: 'Click-Through',
+    //       href: getPermalink('/landing/click-through'),
+    //     },
+    //     {
+    //       text: 'Product Details (or Services)',
+    //       href: getPermalink('/landing/product'),
+    //     },
+    //     {
+    //       text: 'Coming Soon or Pre-Launch',
+    //       href: getPermalink('/landing/pre-launch'),
+    //     },
+    //     {
+    //       text: 'Subscription',
+    //       href: getPermalink('/landing/subscription'),
+    //     },
+    //   ],
+    // },
+
     {
-      text: 'Widgets',
+      text: 'Otros',
       href: '#',
+      links: [
+        {
+          text: 'Planes',
+          href: getPermalink('/#precios'),
+        },
+        {
+          text: 'Preguntas frecuentes',
+          href: getPermalink('/#precios'),
+        },
+        {
+          text: 'FAQ',
+          href: getPermalink('/#faq'),
+        },
+        {
+          text: 'Testimonios',
+          href: getPermalink('/#testimonios'),
+        },
+      ]
     },
   ],
-  actions: [{ text: 'Iniciar Sesión', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
+  actions: [{ text: 'Iniciar Sesión', href: 'https://arxatec.net/auth/views/login.php', target: '_blank' }],
 };
 
 export const footerData = {
@@ -167,7 +211,7 @@ export const footerData = {
   ],
   footNote: `
     <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="https://arxatec.com/favicon/favicon-32x32.png" alt="Arxatec logo" loading="lazy"></img>
-    Hecho por <a class="text-blue-600 underline dark:text-muted" href="#"> Arxatec</a> · Todos los derechos reservados.
+    Hecho por <a class="text-blue-600 underline dark:text-muted" href="#"> ArxaTEC</a> · Todos los derechos reservados.
   `,
 };
 
