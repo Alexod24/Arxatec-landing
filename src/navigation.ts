bf83810132858f66623034331e59896be11e2ad4
Alexod24 <alex.od2408@gmail.com>
2024-11-03 19:26:46 -0500

    feat: :zap: Correccion de errores

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,6 +3,11 @@ import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
 export const headerData = {
   links: [
     {
+      text: 'Inicio',
+      href: getPermalink('/#index'),
+      
+    },
+    {
       text: 'Sobre Nosotros',
       links: [
         {
